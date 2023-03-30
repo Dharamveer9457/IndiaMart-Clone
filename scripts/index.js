1dda35c51d336f369fdf056b04d0bf88dd225e01
Dharamveer <dharammveer945734@outlook.com>
2023-03-30 21:50:14 +0530

    day-3 work of home page

---
+++ scripts/index.js
@@ -0,0 +1,18 @@
+
+let img1 = "https://hm.imimg.com/imhome_gifs/home-banner-4.jpg"
+let img2 =  "https://hm.imimg.com/imhome_gifs/passion-business.jpg"
+
+var slideshowimages = [img1,img2]
+
+let slideshowBox = document.querySelector("#slideshow")
+let img = document.createElement('img')
+
+let i=0;
+setInterval(function(){
+    img.src = slideshowimages[i]
+    i++
+    if(i>=slideshowimages.length){
+        i=0
+    }
+    slideshowBox.appendChild(img)
+},1500)
